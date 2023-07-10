2f70d6ae4b97abb1608b65cf996420309a2f4c96
Norbey Danilo <24207969+ndmc98@users.noreply.github.com>
2023-07-10 11:16:51 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-ManyToMany-example
---
+++ src/main/java/com/example/spring/hibernate/manytomany/repository/TutorialRepository.java
@@ -0,0 +1,15 @@
+package com.example.spring.hibernate.manytomany.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.spring.hibernate.manytomany.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+
+  List<Tutorial> findByTitleContaining(String title);
+
+  List<Tutorial> findTutorialsByTagsId(Long tagId);
+}
